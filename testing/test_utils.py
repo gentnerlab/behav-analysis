f0a5b44229f36318fa0f822703584c2698e6e7d2
fasciae <michael.e.turvey@gmail.com>
2018-09-27 16:23:08 -0700

    made yml & requirements & 1.2 tests

---
+++ testing/test_utils.py
@@ -0,0 +1,16 @@
+import behav
+
+starsTest = []
+for val in [0.0009, 0.009, 0.04, 0.09, 1]:
+	starsTest.append(stars(val))
+assert starsTest == ['***', '**', '*', '.', 'n.s.']
+"""
+assert binP(3, 0.75, 5, 10) == 
+
+q = 0.75/0.
+k = 0.0 #increments if 
+v = 1.0 #increment size
+s = 0.0 #increments if k is between x1 and x2
+tot = 0.0 #step counter
+
+k = 0 tot = 1 """
