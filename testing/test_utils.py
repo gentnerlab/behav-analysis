844b9e04582b4076d9329d273b8cdddc21c8a5c3
fasciae <michael.e.turvey@gmail.com>
2018-10-02 05:03:35 -0700

    made yml & requirements & 1.2 tests (#3)
    * made yml & requirements & 1.2 tests
    
    * bit of clean up
    
    * import fix
    
    * behav cov
    
    * remove comment

---
+++ testing/test_utils.py
@@ -0,0 +1,8 @@
+from behav import utils
+
+
+def test_stars():
+    starsTest = []
+    for val in [0.0009, 0.009, 0.04, 0.09, 1]:
+        starsTest.append(utils.stars(val))
+    assert starsTest == ['***', '**', '*', '.', 'n.s.']
