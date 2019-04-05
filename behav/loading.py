9a07977de572094f13e823acaa3da9eed2895cf5
timsainb <timsainb@gmail.com>
2019-04-05 06:49:32 -0700

    update lr mapping

--- behav/loading.py
+++ behav/loading.py
@@ -162,8 +162,10 @@ def load_data_pandas(subjects, data_folder, force_boolean=['reward']):
                     df_set[broken_df].index = [_validate_time(i,"%Y-%m-%d %H:%M:%S.%f") for i in df_set[broken_df].index]
                     df_set[broken_df] = df_set[broken_df][df_set[broken_df].index != False]
                     df_set[broken_df].index = pd.to_datetime(df_set[broken_df].index)
-    
-            behav_data[subj] = _leftright_to_LR(pd.concat(df_set).sort_index())
+            
+            # concatenate data and remap response left/right to L/R
+            df = pd.concat(df_set).sort_index()['response'].map({'left':'L', 'right':'R'})
+            behav_data[subj] = df
         else:
             print('data not found for %s' % (subj))
     if force_boolean:
